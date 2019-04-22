122f1aa3cccb5dafbaa8fc29cefc773a1fb6a68d
Takahiro Ishikawa <t-ishikawa@i-enter.co.jp>
2019-04-22 18:54:43 +0900

    updated "GameScreen"

--- core/src/jp/techacademy/takahiro/ishikawa/jumpactiongame/GameScreen.kt
+++ core/src/jp/techacademy/takahiro/ishikawa/jumpactiongame/GameScreen.kt
@@ -1,6 +1,5 @@
 package jp.techacademy.takahiro.ishikawa.jumpactiongame
 
-
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.ScreenAdapter
 import com.badlogic.gdx.graphics.GL20
@@ -9,21 +8,40 @@ import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.utils.viewport.FitViewport
 import com.badlogic.gdx.graphics.OrthographicCamera
+import java.util.*
 
 class GameScreen(private val mGame: JumpActionGame) : ScreenAdapter() {
+
     companion object {
-        const val CAMERA_WIDTH = 10f
-        const val CAMERA_HEIGHT = 15f
+        val CAMERA_WIDTH = 10f
+        val CAMERA_HEIGHT = 15f
+        val WORLD_WIDTH = 10f
+        val WORLD_HEIGHT = 15 * 20    // 20画面分登れば終了
+
+        val GAME_STATE_READY = 0
+        val GAME_STATE_PLAYING = 1
+        val GAME_STATE_GAMEOVER = 2
+
+        // 重力
+        val GRAVITY = -12
     }
 
     private val mBg: Sprite
     private val mCamera: OrthographicCamera
     private val mViewPort: FitViewport
 
+    private var mRandom: Random
+    private var mSteps: ArrayList<Step>
+    private var mStars: ArrayList<Star>
+    private lateinit var mUfo: Ufo
+    private lateinit var mPlayer: Player
+
+    private var mGameState: Int
+
     init {
         // 背景の準備
         val bgTexture = Texture("back.png")
-        // TextureRegionで切り出す時の原点は左下
+        // TextureRegionで切り出す時の原点は左上
         mBg = Sprite(TextureRegion(bgTexture, 0, 0, 540, 810))
         mBg.setSize(CAMERA_WIDTH, CAMERA_HEIGHT)
         mBg.setPosition(0f, 0f)
@@ -32,9 +50,20 @@ class GameScreen(private val mGame: JumpActionGame) : ScreenAdapter() {
         mCamera = OrthographicCamera()
         mCamera.setToOrtho(false, CAMERA_WIDTH, CAMERA_HEIGHT)
         mViewPort = FitViewport(CAMERA_WIDTH, CAMERA_HEIGHT, mCamera)
+
+        // プロパティの初期化
+        mRandom = Random()
+        mSteps = ArrayList<Step>()
+        mStars = ArrayList<Star>()
+        mGameState = GAME_STATE_READY
+
+        createStage()
     }
 
     override fun render(delta: Float) {
+        // それぞれの状態をアップデートする
+        update(delta)
+
         Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
@@ -44,14 +73,83 @@ class GameScreen(private val mGame: JumpActionGame) : ScreenAdapter() {
 
         mGame.batch.begin()
 
+        // 背景
         // 原点は左下
         mBg.setPosition(mCamera.position.x - CAMERA_WIDTH / 2, mCamera.position.y - CAMERA_HEIGHT / 2)
         mBg.draw(mGame.batch)
+
+        // Step
+        for (i in 0 until mSteps.size) {
+            mSteps[i].draw(mGame.batch)
+        }
+
+        // Star
+        for (i in 0 until mStars.size) {
+            mStars[i].draw(mGame.batch)
+        }
+
+        // UFO
+        mUfo.draw(mGame.batch)
+
+        //Player
+        mPlayer.draw(mGame.batch)
 
         mGame.batch.end()
     }
 
     override fun resize(width: Int, height: Int) {
         mViewPort.update(width, height)
+    }
+
+    // ステージを作成する
+    private fun createStage() {
+
+        // テクスチャの準備
+        val stepTexture = Texture("step.png")
+        val starTexture = Texture("star.png")
+        val playerTexture = Texture("uma.png")
+        val ufoTexture = Texture("ufo.png")
+
+        // StepとStarをゴールの高さまで配置していく
+        var y = 0f
+
+        val maxJumpHeight = Player.PLAYER_JUMP_VELOCITY * Player.PLAYER_JUMP_VELOCITY / (2 * -GRAVITY)
+        while (y < WORLD_HEIGHT - 5) {
+            val type = if(mRandom.nextFloat() > 0.8f) Step.STEP_TYPE_MOVING else Step.STEP_TYPE_STATIC
+            val x = mRandom.nextFloat() * (WORLD_WIDTH - Step.STEP_WIDTH)
+
+            val step = Step(type, stepTexture, 0, 0, 144, 36)
+            step.setPosition(x, y)
+            mSteps.add(step)
+
+            if (mRandom.nextFloat() > 0.6f) {
+                val star = Star(starTexture, 0, 0, 72, 72)
+                star.setPosition(step.x + mRandom.nextFloat(), step.y + Star.STAR_HEIGHT + mRandom.nextFloat() * 3)
+                mStars.add(star)
+            }
+
+            y += (maxJumpHeight - 0.5f)
+            y -= mRandom.nextFloat() * (maxJumpHeight / 3)
+        }
+
+        // Playerを配置
+        mPlayer = Player(playerTexture, 0, 0, 72, 72)
+        mPlayer.setPosition(WORLD_WIDTH / 2 - mPlayer.width / 2, Step.STEP_HEIGHT)
+
+        // ゴールのUFOを配置
+        mUfo = Ufo(ufoTexture, 0, 0, 120, 74)
+        mUfo.setPosition(WORLD_WIDTH / 2 - Ufo.UFO_WIDTH / 2, y)
+    }
+
+    // それぞれのオブジェクトの状態をアップデートする
+    private fun update(delta: Float) {
+        when (mGameState) {
+            GAME_STATE_READY ->
+                return
+            GAME_STATE_PLAYING ->
+                return
+            GAME_STATE_GAMEOVER ->
+                return
+        }
     }
 }
