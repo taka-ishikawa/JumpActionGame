6bd31da4f13197580b9f34881404a8f702e08225
Takahiro Ishikawa <t-ishikawa@i-enter.co.jp>
2019-04-23 15:46:47 +0900

    [add] Enemy class
    just created Enemy class

---
+++ core/src/jp/techacademy/takahiro/ishikawa/jumpactiongame/Enemy.kt
@@ -0,0 +1,7 @@
+package jp.techacademy.takahiro.ishikawa.jumpactiongame
+
+import com.badlogic.gdx.graphics.Texture
+
+class Enemy(texture: Texture?, srcX: Int, srcY: Int, srcWidth: Int, srcHeight: Int)
+    :GameObject(texture, srcX, srcY, srcWidth, srcHeight) {
+}
