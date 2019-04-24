231f1f4a5f78aa540e49aa7407d22c24be09523a
Takahiro Ishikawa <t-ishikawa@i-enter.co.jp>
2019-04-24 10:43:26 +0900

    [updated] Enemy class. Texture!/ENEMY_WIDTH = 0.8f/update()
    brushed up.

--- core/src/jp/techacademy/takahiro/ishikawa/jumpactiongame/Enemy.kt
+++ core/src/jp/techacademy/takahiro/ishikawa/jumpactiongame/Enemy.kt
@@ -2,12 +2,12 @@ package jp.techacademy.takahiro.ishikawa.jumpactiongame
 
 import com.badlogic.gdx.graphics.Texture
 
-class Enemy(type: Int, texture: Texture?, srcX: Int, srcY: Int, srcWidth: Int, srcHeight: Int)
+class Enemy(type: Int, texture: Texture, srcX: Int, srcY: Int, srcWidth: Int, srcHeight: Int)
     : GameObject(texture, srcX, srcY, srcWidth, srcHeight) {
 
     companion object {
         // 横幅、高さ
-        val ENEMY_WIDTH = 1.0f
+        val ENEMY_WIDTH = 0.8f
         val ENEMY_HEIGHT = 0.8f
 
         // 状態
@@ -31,6 +31,22 @@ class Enemy(type: Int, texture: Texture?, srcX: Int, srcY: Int, srcWidth: Int, s
         mType = type
         if (mType == ENEMY_TYPE_MOVING) {
             velocity.x = ENEMY_VELOCITY
+        }
+    }
+
+    // 座標を更新する
+    fun update(deltaTime: Float) {
+        if (mType == ENEMY_TYPE_MOVING) {
+            x += velocity.x * deltaTime
+
+            if (x < ENEMY_WIDTH / 2) {
+                velocity.x = -velocity.x
+                x = ENEMY_WIDTH / 2
+            }
+            if (x > GameScreen.WORLD_WIDTH - ENEMY_WIDTH / 2) {
+                velocity.x = -velocity.x
+                x = GameScreen.WORLD_WIDTH - ENEMY_WIDTH / 2
+            }
         }
     }
 
